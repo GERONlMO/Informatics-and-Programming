dcbbb681f2164b380b11f3ad7400a403610bb533
Ivan_Novostroev <novostroievv@mail.ru>
2021-09-23 17:39:48 +0300

    Добавлена задача #2

--- Task_0_HW/Task_0_HW/Task_0_HW/main.c
+++ Task_0_HW/Task_0_HW/Task_0_HW/main.c
@@ -1,41 +1,43 @@
 #include <math.h>
 #include <stdio.h>
+#include <locale.h>
 
 int main() {
+	char* locale = setlocale(LC_ALL, "");
 
 	float coorX_1, coorY_1, r1, coorX_2, coorY_2, r2;
 	float d;
-	printf("Vvedite coordinat okryz 1:\nx = ");
+	printf("Введите координаты окружности 1:\nx = ");
 	scanf_s("%f", &coorX_1);
 	printf("y = ");
 	scanf_s("%f", &coorY_1);
-	printf("Vvedite radius okryz 1:\nr = ");
+	printf("Введите радиус окружности 1:\nr = ");
 	scanf_s("%f", &r1);
 
-	printf("Vvedite coordinat okryz 2:\nx = ");
+	printf("Введите координаты окружности 2:\nx = ");
 	scanf_s("%f", &coorX_2);
 	printf("y = ");
 	scanf_s("%f", &coorY_2);
-	printf("Vvedite radius okryz 2:\nr = ");
+	printf("Введите радиус окружности 2:\nr = ");
 	scanf_s("%f", &r2);
 
 	d = sqrt(pow(coorX_2 - coorX_1, 2) + pow(coorY_2 - coorY_1, 2));
 
 	if (d == r1 + r2) {
-		printf("Kasaetsy");
+		printf("Окружности касаются");
 	}
 	if (d < r1 + r2) {
 		if (d == 0 && r1 == r2) {
-			printf("Covpadaet");
+			printf("Окружности совпадают");
 		}
 		else if (d + r1 < r2 || d + r2 < r1) {
-			printf("Odna v drygoi");
+			printf("Одна окружность находится в другой");
 		}
 		else {
-			printf("Peresekaet");
+			printf("Одна окружность пересекает другую");
 		}
 	}
 	if (d > r1 + r2) {
-		printf("Nnesvyzan");
+		printf("Окружности не связаны");
 	}
 }
